25e49ac3908cb213a2ef50152cb44314c5ca49ff
Andreas-206 <puma_real@ukr.net>
2024-04-21 16:34:10 +0300

    +

--- src/contacts.js
+++ src/contacts.js
@@ -1,22 +1,61 @@
-// contacts.js
+import path from 'node:path'
+import * as fs from 'node:fs/promises'
+import crypto from 'node:crypto'
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.resolve('db', 'contacts.json')
 
 async function listContacts() {
-	// ...твій код. Повертає масив контактів.
+	const data = await fs.readFile(contactsPath, { encoding: 'utf-8' })
+
+	return JSON.parse(data)
+}
+
+function writeContacts(contacts) {
+	return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2))
+}
+
+async function getListContacts() {
+	const contacts = await listContacts()
+
+	return contacts
 }
 
 async function getContactById(contactId) {
-	// ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+	const contacts = await listContacts()
+
+	const contact = contact.find(contact => contact.id === id)
+
+	if (typeof contact === 'undefined') {
+		return null
+	}
+
+	return contact
 }
 
 async function removeContact(contactId) {
-	// ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+	const contacts = await listContacts()
+
+	const index = contacts.findIndex(contact => contact.id === contactId)
+
+	if (index === -1) {
+		return null
+	}
+
+	const removedContact = contacts[index]
+	contacts.splice(index, 1)
+	await writeContacts(contacts)
+
+	return removedContact
 }
 
 async function addContact(name, email, phone) {
-	// ...твій код. Повертає об'єкт доданого контакту (з id).
+	const contacts = await listContacts()
+
+	const newContact = { ...contact, id: crypto.randomUUID() }
+	contacts.push(newContact)
+	await writeContacts(contacts)
+
+	return newContact
 }
+
+export default { getListContacts, getContactById, removeContact, addContact }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import { program } from 'commander'
-program
-	.option('-a, --action <type>', 'choose action')
-	.option('-i, --id <type>', 'user id')
-	.option('-n, --name <type>', 'user name')
-	.option('-e, --email <type>', 'user email')
-	.option('-p, --phone <type>', 'user phone')
-
-program.parse()
+import Contacts from './contacts'
 
 const options = program.opts()
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
 	switch (action) {
 		case 'list':
-			// ...
+			const contacts = await Contacts.getListContacts()
+			return contacts
 			break
 
 		case 'get':
-			// ... id
+			const contact = await Contacts.getContactById(id)
+			return contact
 			break
 
 		case 'add':
-			// ... name email phone
+			const addContact = await Contacts.addContact({ name, email, phone })
+			return addContact
 			break
 
 		case 'remove':
-			// ... id
+			const removedContact = await Contacts.removeContact(id)
+			return removedContact
 			break
 
 		default:
@@ -34,4 +30,13 @@ async function invokeAction({ action, id, name, email, phone }) {
 	}
 }
 
-invokeAction(options)
+program
+	.option('-a, --action <type>', 'choose action')
+	.option('-i, --id <type>', 'user id')
+	.option('-n, --name <type>', 'user name')
+	.option('-e, --email <type>', 'user email')
+	.option('-p, --phone <type>', 'user phone')
+
+program.parse(process.argv)
+
+invokeAction(program.opts()).then(console.log).catch(console.error)
